bdc8ac4f1eae4bd43c90ce56137d2c7bb0548de8
Alejandro Lopez Velez <Inglopezvelez@hotmail.com>
2021-12-04 21:01:03 -0500

    Heroku deploy

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/*',function(req, res){
+    res.sendFile(path.join(__dirname +'/dist/frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
